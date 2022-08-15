516c0ec7eb473e57a0ff54153caa74d0eb2c9c6a
Fogel Egor <fogel_92@bk.ru>
2022-08-16 02:03:05 +0700

    feat: add count of correct variants

--- src/App.js
+++ src/App.js
@@ -24,11 +24,13 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
       <button>Попробовать снова</button>
     </div>
   );
@@ -56,16 +58,25 @@ function Game({ step, question, onClickVariant }) {
 
 function App() {
   const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+
   const question = questions[step];
 
   const onClickVariant = (i) => {
     setStep(step + 1);
+
+    if (questions.correct === i) {
+      setCorrect(correct + 1);
+    }
   };
 
   return (
     <div className="App">
-      <Game step={step} question={question} onClickVariant={onClickVariant} />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
