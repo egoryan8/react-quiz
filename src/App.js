b6448782f3ffbbc869eca6f5e8c29ed2f0cee091
Fogel Egor <fogel_92@bk.ru>
2022-08-16 01:50:35 +0700

    feat: add questions crossing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './index.scss';
 
 const questions = [
@@ -32,26 +34,35 @@ function Result() {
   );
 }
 
-function Game() {
+function Game({ question, onClickVariant }) {
   return (
     <>
       <div className="progress">
         <div style={{ width: '50%' }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, i) => (
+          <li onClick={() => onClickVariant(i)} key={text}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (i) => {
+    setStep(step + 1);
+  };
+
   return (
     <div className="App">
-      <Game />
+      <Game question={question} onClickVariant={onClickVariant} />
       {/* <Result /> */}
     </div>
   );
